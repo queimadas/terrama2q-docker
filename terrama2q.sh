96790574aa290e3e347522c3a517832d44d1c74d
jonatas.simoes <jonatas.simoes@bbe9ffdc-cc58-4e53-b885-bfd695a85e55>
2019-02-20 19:50:28 +0000

    Separa declaração de variáveis
    git-svn-id: https://svn.cptec.inpe.br/qmd-terrama2q/qmd-terrama2q-docker/trunk@15 bbe9ffdc-cc58-4e53-b885-bfd695a85e55

--- terrama2q.sh
+++ terrama2q.sh
@@ -18,57 +18,34 @@ function run_into() {
 }
 
 function parse() {
-  echo 'eval $(egrep -v '^\#' .env | xargs)' >  /tmp/tmp.sh
-  echo 'cat <<END_OF_TEXT' >>  /tmp/tmp.sh
-  cat "$1"                 >> /tmp/tmp.sh
-  echo 'END_OF_TEXT'       >> /tmp/tmp.sh
+  truncate /tmp/tmp.sh --size 0
+  echo 'eval $(egrep -v '^\#' .env.default | xargs)' >> /tmp/tmp.sh
+  echo 'eval $(egrep -v '^\#' .env | xargs)' >> /tmp/tmp.sh
+  echo 'cat <<EOF' >> /tmp/tmp.sh
+  cat "$1" >> /tmp/tmp.sh
+  echo 'EOF' >> /tmp/tmp.sh
   bash /tmp/tmp.sh > "$1"
   rm /tmp/tmp.sh
 }
 
-GIT_LINK="https://git-scm.com/book/en/v2/Getting-Started-Installing-Git"
+function apply_context() {
+  eval $(egrep -v '^#' $1 | xargs)
+}
+
+apply_context .env.default
+if [ ! -f '.env' ]; then
+  echo "Arquivo env não existe. Configure-o para prosseguir."
+  exit 1
+fi
+apply_context .env
+
 GIT=$(which git)
-is_valid $? "Git must be installed. ${GIT_LINK}"
-echo "Using git from ${GIT}"
-
-DOCKER_LINK="https://docs.docker.com/install/"
 DOCKER=$(which docker)
-is_valid $? "Docker must be installed. ${DOCKER_LINK}"
-echo "Using docker from ${DOCKER}"
-
-DOCKER_COMPOSE_LINK="https://docs.docker.com/compose/install/"
 DOCKER_COMPOSE=$(which docker-compose)
+
+is_valid $? "Git must be installed. ${GIT_LINK}"
+is_valid $? "Docker must be installed. ${DOCKER_LINK}"
 is_valid $? "Docker-compose must be installed. ${DOCKER_COMPOSE_LINK}"
-echo "Using docker-compose from ${DOCKER_COMPOSE}"
-
-TERRAMA2_REPO_URL="https://github.com/terrama2/docker.git"
-BDQLIGHT_REPO_URL="https://github.com/jonatasleon/bdqueimadas-light.git"
-TERRAMA2_DOCKER_DIR="terrama2-docker"
-BDQLIGHT_DOCKER_DIR="bdqlight"
-
-BDQLIGHT_CONF_DIR="bdqlight-conf"
-NGINX_CONF_DIR="nginx-conf"
-TERRAMA2_CONF_DIR="terrama2-conf"
-
-SHARED_VOLUME="terrama2_shared_vol"
-TERRAMA2_VOLUME="terrama2_data_vol"
-GEOSERVER_VOLUME="terrama2_geoserver_vol"
-POSTGRES_VOLUME="terrama2_pg_vol"
-BDQLIGHT_VOLUME="terrama2_bdq_vol"
-
-TERRAMA2_NETWORK="terrama2_net"
-
-GEOSERVER_IMAGE="terrama2/geoserver:2.11"
-POSTGRES_IMAGE="mdillon/postgis"
-BDQLIGHT_IMAGE="jonatasleon/bdqlight:1.0.1"
-NGINX_IMAGE="nginx:latest"
-
-GEOSERVER_CONTAINER="terrama2_geoserver"
-POSTGRES_CONTAINER="terrama2_pg"
-BDQLIGHT_CONTAINER="terrama2_bdq"
-NGINX_CONTAINER="terrama2_nginx"
-
-POSTGRES_PASSWORD=mysecretpassword
 
 if [ ! -d ${TERRAMA2_DOCKER_DIR} ]; then
   git clone ${TERRAMA2_REPO_URL} ${TERRAMA2_DOCKER_DIR}
@@ -77,12 +54,6 @@ fi
 if [ ! -d ${BDQLIGHT_DOCKER_DIR} ]; then
   git clone ${BDQLIGHT_REPO_URL} ${BDQLIGHT_DOCKER_DIR}
 fi
-
-if [ ! -f '.env' ]; then
-    echo "Arquivo env não existe. Configure-o para prosseguir."
-    exit 1
-fi
-eval $(egrep -v '^#' .env | xargs)
 
 for image in ${TERRAMA2_CONF_DIR}/terrama2_webapp.json.in \
              ${TERRAMA2_CONF_DIR}/terrama2_webmonitor.json; do
@@ -99,7 +70,7 @@ cp ${TERRAMA2_CONF_DIR}/terrama2_webmonitor.json.base \
 cp -r ${TERRAMA2_DOCKER_DIR}/bdqueimadas-light/* ${BDQLIGHT_DOCKER_DIR}/
 run_into ${BDQLIGHT_DOCKER_DIR} "docker build --tag ${BDQLIGHT_IMAGE} . --rm"
 
-docker network create ${TERRAMA2_NETWORK}
+docker network create ${SHARED_NETWORK}
 
 docker volume create ${SHARED_VOLUME}
 docker volume create ${TERRAMA2_VOLUME}
@@ -110,29 +81,32 @@ docker volume create ${BDQLIGHT_VOLUME}
 docker run -d \
            --name ${GEOSERVER_CONTAINER} \
            --restart unless-stopped \
-           --network ${TERRAMA2_NETWORK} \
+           --network ${SHARED_NETWORK} \
            -p 127.0.0.1:8080:8080 \
-           -e "GEOSERVER_URL=/geoserver" \
-           -e "GEOSERVER_DATA_DIR=/opt/geoserver/data_dir" \
+           -e GEOSERVER_URL=${GEOSERVER_URL} \
+           -e GEOSERVER_DATA_DIR=${GEOSERVER_DATA_DIR} \
            -v ${TERRAMA2_VOLUME}:/data \
            -v ${SHARED_VOLUME}:/shared-data \
-           -v ${GEOSERVER_VOLUME}:/opt/geoserver/data_dir \
+           -v ${GEOSERVER_VOLUME}:${GEOSERVER_DATA_DIR} \
            -v ${PWD}/${TERRAMA2_DOCKER_DIR}/conf/terrama2_geoserver_setenv.sh:/usr/local/tomcat/bin/setenv.sh \
            ${GEOSERVER_IMAGE}
 
 docker run -d \
            --name ${POSTGRES_CONTAINER} \
            --restart unless-stopped \
-           --network ${TERRAMA2_NETWORK} \
-           -p 127.0.0.1:5433:5432 \
+           --network ${SHARED_NETWORK} \
+           -p 127.0.0.1:${POSTGRES_PORT}:5432 \
            -v ${POSTGRES_VOLUME}:/var/lib/postgresql/data \
+           -e POSTGRES_USER=${POSTGRES_USER} \
            -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+           -e POSTGRES_USER=${POSTGRES_USER} \
+           -e POSTGRES_DB=${POSTGRES_DB} \
            ${POSTGRES_IMAGE}
 
 docker run -d \
            --name ${BDQLIGHT_CONTAINER} \
            --restart unless-stopped \
-           --network ${TERRAMA2_NETWORK} \
+           --network ${SHARED_NETWORK} \
            -p 127.0.0.1:39000:39000 \
            -v ${PWD}/${BDQLIGHT_CONF_DIR}/:/opt/bdqueimadas-light/configurations/ \
            -v ${PWD}/${TERRAMA2_DOCKER_DIR}/conf/bdqueimadas-light/.pgpass:/root/.pgpass \
@@ -142,7 +116,7 @@ docker run -d \
 docker run -d \
            --name ${NGINX_CONTAINER} \
            --restart unless-stopped \
-           --network ${TERRAMA2_NETWORK} \
+           --network ${SHARED_NETWORK} \
            -p 0.0.0.0:${PUBLIC_PORT}:80 \
            -v ${PWD}/${NGINX_CONF_DIR}/nginx.conf:/etc/nginx/nginx.conf:ro \
            -v ${PWD}/${NGINX_CONF_DIR}/proxy_params:/etc/nginx/proxy_params:ro \
